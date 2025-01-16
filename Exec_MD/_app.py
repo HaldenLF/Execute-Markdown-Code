da81c29f2714323e5bc815b97edead67b78788be
HaldenLF <Liamhalliden20@gmail.com>
2025-01-16 20:14:12 +0800

    Turned module to command line application

---
+++ Exec_MD/_app.py
@@ -0,0 +1,36 @@
+import argparse
+from Exec_MD import parse_text
+import sys
+
+
+def app():
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, help='input markdown file')
+    parser.add_argument('-o', '--output', type=str, help='output markdown file')
+    args = parser.parse_args()
+    
+    path_in = args.input
+    path_out = args.output
+    
+    # If not input file was given 
+    # read from stdin
+    if path_in is None:
+        md_in = ''
+        for line in sys.stdin:
+            md_in += line
+    # read from file
+    else:
+        with open(path_in) as fd:
+            md_in = fd.read()
+    
+    md_out = parse_text(md_in)
+    
+    # if no output file was given 
+    # write to stdout
+    if path_out is None:
+        print(md_out)
+    # write to output file
+    else:
+        with open(path_out, 'w') as fd:
+            fd.write(md_out)    
